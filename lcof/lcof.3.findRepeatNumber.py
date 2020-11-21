0e8956e0ae1031a98967ed95339008ddd4cb8306
oriys <Kirin.Toudou@protonmail.com>
2020-11-21 20:41:27 +0800

    🌧️

---
+++ lcof/lcof.3.findRepeatNumber.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        s = set()
+        for num in nums:
+            if num in s:
+                return num
+            s.add(num)
